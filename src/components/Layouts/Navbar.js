4b02967cd06fddb40dd3592a45190e0ff50d680d
Rony Dev <ronyfonsecadelima24@gmail.com>
2025-02-18 19:43:44 -0300

    Mechendo na responsividade

--- src/components/Layouts/Navbar.js
+++ src/components/Layouts/Navbar.js
@@ -15,23 +15,26 @@ function Navbar(){
                 <img src={Logo} alt="Logo"/>
                 <h2>Get a Pet</h2>
             </div>
-            <ul>
-                <li>
-                    <Link to="/">Adotar</Link>
-                </li>
-                {loged ? (<>
-                <li><Link to="/pet/mypets">Meus pets</Link></li>
-                <li><Link to="/pet/myadoptions">Minhas adoções</Link></li>
-                <li><Link to="/user/profile">Perfil</Link></li>
-                <li onClick={logout}>Sair</li>
-                </>) : (<>
-                <li>
-                    <Link to="/Login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/Register">Register</Link>
-                </li></>)}
-            </ul>
+            <div className={styles.menu_cell}>
+                <p>Ver mais</p>
+                <ul className={styles.links}>
+                    <li>
+                        <Link to="/">Adotar</Link>
+                    </li>
+                    {loged ? (<>
+                    <li><Link to="/pet/mypets">Meus pets</Link></li>
+                    <li><Link to="/pet/myadoptions">Minhas adoções</Link></li>
+                    <li><Link to="/user/profile">Perfil</Link></li>
+                    <li onClick={logout}>Sair</li>
+                    </>) : (<>
+                    <li>
+                        <Link to="/Login">Login</Link>
+                    </li>
+                    <li>
+                        <Link to="/Register">Register</Link>
+                    </li></>)}
+                </ul>
+            </div>
         </nav>
     )
 }
